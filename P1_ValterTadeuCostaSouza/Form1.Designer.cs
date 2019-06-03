7e234730a45bab77f8b8c5d02d1c3afa74285c36
Tadeu Costa <tadeucohen@hotmail.com>
2019-06-03 11:36:36 -0300

    commitando o projeto de calculos de médias

---
+++ P1_ValterTadeuCostaSouza/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace P1_ValterTadeuCostaSouza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbMaiorAltura = new System.Windows.Forms.Label();
+            this.lbIdade = new System.Windows.Forms.Label();
+            this.lbIdade2 = new System.Windows.Forms.Label();
+            this.lbMedia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(485, 47);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 0;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(485, 114);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(485, 187);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 0;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(72, 67);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(59, 22);
+            this.txtIdade.TabIndex = 1;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(218, 67);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(59, 22);
+            this.txtAltura.TabIndex = 1;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(355, 67);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(59, 22);
+            this.txtPeso.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(48, 126);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(109, 212);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(199, 126);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(109, 212);
+            this.listBox2.TabIndex = 2;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(338, 126);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(109, 212);
+            this.listBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Idade :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = " Altura :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(303, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Peso :";
+            // 
+            // lbMaiorAltura
+            // 
+            this.lbMaiorAltura.AutoSize = true;
+            this.lbMaiorAltura.Location = new System.Drawing.Point(48, 367);
+            this.lbMaiorAltura.Name = "lbMaiorAltura";
+            this.lbMaiorAltura.Size = new System.Drawing.Size(46, 17);
+            this.lbMaiorAltura.TabIndex = 4;
+            this.lbMaiorAltura.Text = "label4";
+            // 
+            // lbIdade
+            // 
+            this.lbIdade.AutoSize = true;
+            this.lbIdade.Location = new System.Drawing.Point(48, 401);
+            this.lbIdade.Name = "lbIdade";
+            this.lbIdade.Size = new System.Drawing.Size(46, 17);
+            this.lbIdade.TabIndex = 4;
+            this.lbIdade.Text = "label4";
+            // 
+            // lbIdade2
+            // 
+            this.lbIdade2.AutoSize = true;
+            this.lbIdade2.Location = new System.Drawing.Point(48, 435);
+            this.lbIdade2.Name = "lbIdade2";
+            this.lbIdade2.Size = new System.Drawing.Size(0, 17);
+            this.lbIdade2.TabIndex = 4;
+            // 
+            // lbMedia
+            // 
+            this.lbMedia.AutoSize = true;
+            this.lbMedia.Location = new System.Drawing.Point(48, 463);
+            this.lbMedia.Name = "lbMedia";
+            this.lbMedia.Size = new System.Drawing.Size(46, 17);
+            this.lbMedia.TabIndex = 4;
+            this.lbMedia.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 489);
+            this.Controls.Add(this.lbIdade2);
+            this.Controls.Add(this.lbMedia);
+            this.Controls.Add(this.lbIdade);
+            this.Controls.Add(this.lbMaiorAltura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnInserir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbMaiorAltura;
+        private System.Windows.Forms.Label lbIdade;
+        private System.Windows.Forms.Label lbIdade2;
+        private System.Windows.Forms.Label lbMedia;
+    }
+}
+
---
+++ P1_ValterTadeuCostaSouza/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1_ValterTadeuCostaSouza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            if (txtIdade.Text != "" && txtAltura.Text != "" && txtPeso.Text !="")
+            {
+                listBox1.Items.Add(txtIdade.Text);
+                listBox2.Items.Add(txtAltura.Text);
+                listBox3.Items.Add(txtPeso.Text);
+                txtAltura.Clear();
+                txtPeso.Clear();
+                txtIdade.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Insira os dados");
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtPeso.Clear();
+            txtIdade.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            lbMaiorAltura.Text = "";
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double maior_altura = 0;
+            double menor_altura = 0;
+            int peso = 0;
+            int mediapeso = 0;
+            int somaidade = 0;
+            int idade = 0;
+
+            int somaidade2 = 0;
+            int idade2 = 0;
+            int idade3 = 0;
+            maior_altura = Convert.ToDouble(listBox2.Items[0]);
+            menor_altura = Convert.ToDouble(listBox2.Items[0]);
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                double aux = 0;
+
+                aux = Convert.ToDouble(listBox2.Items[i]);
+                if (aux > maior_altura)
+                {
+                    maior_altura = aux;
+
+
+
+                }
+                else if (aux < maior_altura)
+                {
+                    menor_altura = aux;
+                }
+
+
+            }
+
+
+            lbMaiorAltura.Text = " A maior altura é de : " + maior_altura;
+
+
+
+            
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+
+            {
+                
+                idade = Convert.ToInt32(listBox1.Items[i]);
+                if (idade < 35)
+                {
+                    somaidade = somaidade + 1;
+                }
+            }
+
+            lbIdade.Text = " A quantidade de alunos com idade inferior a 35 é de : " + somaidade;
+
+           
+            idade3 = Convert.ToInt32(listBox1.Items[0]);
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+
+            {
+                
+                idade2 = Convert.ToInt32(listBox1.Items[i]);
+                
+                if ((idade2 >= 20)&&(idade2 <=30)) 
+                {
+                    somaidade2 = somaidade2 + 1;
+                }
+            }
+
+            lbIdade2.Text = "Numero de aluno entre 20 e 30 anos é de " + somaidade2;
+
+           
+            
+            for (int i = 0; i < listBox3.Items.Count; i++)
+
+            {
+                peso = Convert.ToInt32(listBox3.Items[i]);
+               
+                if ((idade2 >= 20) && (idade2 <= 30)) 
+                {
+                    
+                    peso = peso + peso;
+                }
+
+            }
+
+            if(somaidade2!= 0)
+            {
+                mediapeso = peso / somaidade2;
+            }
+            
+
+
+
+
+            lbMedia.Text = "Media de Peso com idade entre 20 e 30 anos é de  " + mediapeso;
+
+
+        }
+    }
+    }
+
